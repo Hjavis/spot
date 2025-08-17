04c60b47e0b30126a45ef7628d8f9e7e47577f37
Hjavis <hjaltedv04@gmail.com>
2025-08-17 17:43:17 +0200

    preprocess-data: interpolate numeric forecasts, ffill intraday
    Handled missing values using interpolate for columns where only a few missing values occoured.
    
    For columns with frequent NaN i decided to use Ffill.

--- download_data.py
+++ download_data.py
@@ -1,4 +1,4 @@
-from scrape import fetch_data
+from fetch_data import fetch_data
 
 
 df_spot_prices = fetch_data(
@@ -28,6 +28,7 @@ df_weather_pivot = df_weather_forecast.pivot_table(
     columns='ForecastType',
     values=['ForecastDayAhead','ForecastIntraday','Forecast5Hour','Forecast1Hour']
 )
+
 df_weather_pivot.columns = ['_'.join(col).strip() for col in df_weather_pivot.columns.values]
 df_weather_forecast = df_weather_pivot.reset_index()
 
@@ -35,11 +36,39 @@ df = df_spot_prices.merge(df_weather_forecast, on=['HourUTC','PriceArea'], how='
 
 df.to_csv("df.csv", index=False)
 
+
+#Håndter Missing Values
+df = df.infer_objects()
+
+# Interpolate kun numeriske cols, og ikke intraday_forecast fordi der er rigtig mange NaN. 
+cols_to_interpolate = [
+    'Forecast1Hour_Offshore Wind',
+    'Forecast1Hour_Onshore Wind',
+    'Forecast1Hour_Solar',
+    'Forecast5Hour_Offshore Wind',
+    'Forecast5Hour_Onshore Wind',
+    'Forecast5Hour_Solar',
+    'ForecastDayAhead_Offshore Wind',
+    'ForecastDayAhead_Onshore Wind',
+    'ForecastDayAhead_Solar'
+]
+df[cols_to_interpolate] = df[cols_to_interpolate].interpolate(method='linear', limit_direction='both')
+
+#Intraday forecasts har alt for mange NaN, derfor ffill
+intraday_cols = [
+    'ForecastIntraday_Offshore Wind',
+    'ForecastIntraday_Onshore Wind',
+    'ForecastIntraday_Solar'
+]
+df[intraday_cols] = df[intraday_cols].ffill().bfill() #backfiller kun hvis starten af datasættet har NaN
+
+df.to_csv("df_interpolate.csv", index=False)
+
 #Tjek eventuelt
 #print(df.head(1000))
 #print(df.columns)
 
-#bool = df_weather_forecast.isna().mean()
+#bool = df_weather_forecast.isna()
 #print(bool)
-
-df
+#mean = df_weather_forecast.isna().mean()
+#print(mean)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from download_data import df
+import pandas as pd
 
-print(df.head(5))
+df = pd.read_csv("df.csv")
+df_interpolate = pd.read_csv("df_interpolate.csv")
+
+print(df.head(50))
+print(df_interpolate.head(50))
